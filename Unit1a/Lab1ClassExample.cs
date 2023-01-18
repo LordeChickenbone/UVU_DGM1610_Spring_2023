0017035adabc32ad6bdeecf1b208313196a25478
Shen Schroeppel <122662423+ShenSchr@users.noreply.github.com>
2023-01-18 16:24:07 -0700

    Lab 1 Health edit

--- Unit1a/Lab1ClassExample.cs
+++ Unit1a/Lab1ClassExample.cs
@@ -1,7 +1,7 @@
 public class ClassExample
 {
     public int numberCount;
-
+    public Health playerHealth;
     public void RunExample()
     {
         Console.WriteLine("Hello World? Are you there?");
