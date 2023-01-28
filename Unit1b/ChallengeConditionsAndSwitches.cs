000a1cfa65d5d5c7da6a8bf5d7f52647e1758fec
Shen Schroeppel <122662423+ShenSchr@users.noreply.github.com>
2023-01-28 16:27:04 -0700

    Finished Challenge (untested)

--- Unit1b/ChallengeConditionsAndSwitches.cs
+++ Unit1b/ChallengeConditionsAndSwitches.cs
@@ -4,11 +4,13 @@ public class Program
 {
 	public Combat diceRoll;
 	public AfterCombat rollDice;
+	public WhichEnemy thisEnemy;
 	
 	public void Main()
 	{
 		diceRoll = new Combat();
 		rollDice = new AfterCombat();
+		thisEnemy = new WhichEnemy();
 		
 		Console.WriteLine("Start Attacking!");
 		Console.WriteLine(" ");
@@ -16,16 +18,20 @@ public class Program
 		diceRoll.Attack(12,23);
 		diceRoll.Attack(30,31);
 		Console.WriteLine(" ");
-		if (hit = true)
-		{
-			rollDice.Damage(32,14);
-			rollDice.Damage(45,28);
-			rollDice.Damage(4,4);
-		}
-		else
-		{
-			Console.WriteLine("No Damage");
-		}
+		rollDice.Damage(32,14);
+		rollDice.Damage(45,28);
+		rollDice.Damage(4,4);
+		Console.WriteLine(" ");
+		thisEnemy.currentOpponent = WhichEnemy.Enemies.Goblin;
+		thisEnemy.GetLoot();
+		thisEnemy.currentOpponent = WhichEnemy.Enemies.Orc;
+		thisEnemy.GetLoot();
+		thisEnemy.currentOpponent = WhichEnemy.Enemies.Ogre;
+		thisEnemy.GetLoot();
+		thisEnemy.currentOpponent = WhichEnemy.Enemies.Troll;
+		thisEnemy.GetLoot();
+		thisEnemy.currentOpponent = WhichEnemy.Enemies.Dragon;
+		thisEnemy.GetLoot();
 	}
 }
 
@@ -33,16 +39,6 @@ public class Combat
 {
 	public void Attack (int armorClass, int attackScore)
 	{
-		public bool hit = true;
-		if (armorClass <= attackScore)
-		{
-			hit = true;
-		}
-		else
-		{
-			hit = false;
-		} 
-
 		if (armorClass <= attackScore) 
 		{
 			Console.WriteLine("AC:" + armorClass + " vs. " + "Attack:" + attackScore);
@@ -68,6 +64,45 @@ public class AfterCombat
 		else 
 		{
 			Console.WriteLine("Took " + damagePoints + " damage. You are dead!");
+		}
+	}
+}
+
+public class WhichEnemy
+{
+	public enum Enemies
+	{
+		Goblin,
+		Orc,
+		Ogre,
+		Troll,
+		Dragon,
+	}
+	
+	public Enemies currentOpponent = Enemies.Troll;
+	
+	public void GetLoot ()
+	{
+		switch (currentOpponent) 
+		{
+			case Enemies.Goblin:
+				Console.WriteLine("Goblin killed! Loot:5 gold");
+				break;
+			case Enemies.Orc:
+				Console.WriteLine("Orc defeated! Loot:25 gold");
+				break;
+			case Enemies.Ogre:
+				Console.WriteLine("Ogre beaten! Loot:100 gold");
+				break;
+			case Enemies.Troll:
+				Console.WriteLine("Troll evicerated! Loot:500 gold");
+				break;
+			case Enemies.Dragon:
+				Console.WriteLine("Dragon slain! Loot:10,000 gold");
+				break;
+			default:
+				Console.WriteLine("No combatants.");
+				break;
 		}
 	}
 }
