05bcf1ba3fb5bcaa863f52aa24d69de1ed3af79b
Shen Schroeppel <shenschr@gmail.com>
2023-04-06 20:39:44 -0600

    "Completed" Unit7a Challenge

--- Matching Game/Assets/Scripts/TransformBehavior.cs
+++ Matching Game/Assets/Scripts/TransformBehavior.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TransformBehavior : MonoBehaviour
 {
+    private Vector3 offScreen = new Vector3(10, 0, 0);
+    public void MoveOffScreen()
+    {
+        transform.position = offScreen;
+    }
     public void ResetToZero()
     {
         transform.position = Vector3.zero;
